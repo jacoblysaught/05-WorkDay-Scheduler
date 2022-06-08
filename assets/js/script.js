4d898f196e2713e08dcb2b99700859d9e8cdd435
Jacob Lysaught <jacob.lysaught@gmail.com>
2022-06-08 17:12:25 -0600

    added placeholder to textarea and began function for bgcolors based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 // sets the current date at the top of the page
-var today = moment();
-$("#currentDay").text(today.format("MMMM Do YYYY, h:mm:ss a"));
+var now = moment();
+$("#currentDay").text(now.format("MMMM Do YYYY, h:mm:ss a"));
 
+// 
 window.setInterval(function () {
     $('#currentDay').html(moment().format("MMMM Do YYYY, h:mm:ss a"))
 }, 1000);
+
+
+// updates the background of each row based on the time of day
+var colorTasks = function() {
+
+    var currentHour = moment().hour();
+    $(textarea).each(function() {
+        var elementHour = parseInt($(this).attr("element"));
+
+        // sets the bg color depending on past, present, and future
+        if (elementHour < currentHour) {
+            $(this).removeClass(["present", "future"]).addClass("past");
+        }
+        else if ( elementHour === currentHour) {
+            $(this).removeClass(["future", "past"]).addClass("present");
+        }
+        else {
+            $(this).removeClass(["past", "present"]).addClass("future");
+        }
+    })
+};
